223a8f9106ccaed1fe1fa72b9c766ba30151105b
sunnysomething-code <82kjbvq65n@privaterelay.appleid.com>
2026-08-13 15:13:02 +1000

    Port 26.1.2: complete EnderDragonFight mixin rename

--- src/main/java/qouteall/imm_ptl/core/mixin/common/miscellaneous/IEEndDragonFight.java
+++ src/main/java/qouteall/imm_ptl/core/mixin/common/miscellaneous/IEEndDragonFight.java
@@ -5,11 +5,11 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
-@Mixin(EndDragonFight.class)
+@Mixin(EnderDragonFight.class)
 public interface IEEndDragonFight {
     @Accessor("needsStateScanning")
     boolean ip_getNeedsStateScanning();
-    
+
     @Invoker("scanState")
     void ip_scanState();
 }
